3e5bfffc024ce736e310adfc923894fad4ccb9d1
Jean Stephane <jsa1512@gmail.com>
2026-06-13 23:47:12 +0200

    Test Git reussi

--- src/swing003/Principale.java
+++ src/swing003/Principale.java
@@ -7,6 +7,7 @@ public class Principale {
 		System.out.println("Hello world !");
 		System.out.println("Belle journée !");
 		System.out.println("test Git");
+		System.out.println("test Git réussi");
 
 	}
 
