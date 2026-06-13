97dc8dad17e053ce122813b95a862b00959da99b
jsa1512 <jsa1512@gmail.com>
2026-06-13 18:06:57 +0200

    Test Git

--- src/swing003/Principale.java
+++ src/swing003/Principale.java
@@ -6,6 +6,7 @@ public class Principale {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world !");
 		System.out.println("Belle journée !");
+		System.out.println("test Git");
 
 	}
 
